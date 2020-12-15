e4f901b7376d35deaa419f35303e0fe102248a55
Maxwell Hubbard <hubs.maxwell@gmail.com>
2020-12-15 17:05:04 -0500

    Initial Commit

---
+++ HackUMass2020/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackUMass2020
+//
+//  Created by Maxwell Hubbard on 12/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
